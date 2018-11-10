2e3145b20bff854ffa8baa8c3cf0ff35e42eb6f1
Maria Chobanu <Marychobanu@gmail.com>
2018-11-10 12:02:06 +0300

    Точность 62%

--- Spectra Net/Spectra_Net.py
+++ Spectra Net/Spectra_Net.py
@@ -23,7 +23,7 @@ images = np.array(images)
 images = images.reshape(len(stars), 75625)
 
 from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(images, labels, test_size=0.1, random_state=4)
+X_train, X_test, y_train, y_test = train_test_split(images, labels, test_size=0.1, random_state=42)
 
 
 from keras.models import Sequential
